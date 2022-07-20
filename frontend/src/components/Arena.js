24f3e1e4a872de41bb1c53bf3d11a2991249ffd3
cryptoguru1012 <tiop08317@gmail.com>
2022-07-20 01:01:17 -0400

    implement all functions

--- frontend/src/components/Arena.js
+++ frontend/src/components/Arena.js
@@ -1,21 +1,31 @@
 import { useEffect, useState } from "react";
 import ArenaBackground from "../assets/arena-bg.svg";
 import BattleBackground from "../assets/battle-bg.svg";
+import FireAnimation from "../assets/fire.gif";
+import FightingSound from "../assets/fighting.wav";
 
 function Arena({ info, mintInfo }) {
   const [players, setPlayers] = useState([]);
   const [aliveRabbits, setAliveRabbits] = useState([]);
   const [aliveTurtles, setAliveTurtles] = useState([]);
+  const [deadRabbits, setDeadRabbits] = useState([]);
+  const [deadTurtles, setDeadTurtles] = useState([]);
   const [deadItems, setDeadItems] = useState([]);
   const [loading, setLoading] = useState(false);
   const [selectedRabbit, setSelectedRabbit] = useState();
   const [selectedTurtle, setSelectedTurtle] = useState();
   const [isOpenLeaderboard, setIsOpenLeaderBoard] = useState(false);
+  const [isFighting, setIsFighting] = useState(false);
+  const [turtleFilter, setTurtleFilter] = useState("alive");
+  const [rabbitFilter, setRabbitFilter] = useState("alive");
+  const audio = new Audio(FightingSound);
 
   const getData = async () => {
     setLoading(true);
     const rabbits = [];
     const turtles = [];
+    const deadrabbits = [];
+    const deadturtles = [];
     const dead = [];
     const players = [];
     for (let i = 1; i < mintInfo.gameInfo[1] + mintInfo.gameInfo[0]; i++) {
@@ -32,13 +42,19 @@ function Arena({ info, mintInfo }) {
         }
       } else {
         dead.push(result);
+        if (type == "Turtle") {
+          deadturtles.push(result);
+        } else {
+          deadrabbits.push(result);
+        }
       }
       const player = await info.contract.methods.getPlayerByIndex(i).call();
-      console.log(player);
       players.push(player);
     }
     setAliveRabbits(rabbits);
     setAliveTurtles(turtles);
+    setDeadRabbits(deadrabbits);
+    setDeadTurtles(deadturtles);
     setDeadItems(dead);
     setPlayers(players);
     setLoading(false);
@@ -48,43 +64,74 @@ function Arena({ info, mintInfo }) {
     info && info.contract && mintInfo && getData();
   }, [info, mintInfo])
 
+  const startFighting = () => {
+    setIsFighting(true);
+    audio.play();
+    const timer = setTimeout(() => {
+      setIsFighting(false);
+      audio.pause();
+    }, 3000);
+    return () => clearTimeout(timer);
+  }
+
   return (
     <div style={{ position: "relative", backgroundImage: `url(${ArenaBackground})`, backgroundSize: "100% 100%", width: "1146px", display: "flex", flexDirection: "column", alignItems: "center", padding: "188px 0" }}>
       <div style={{ backgroundColor: "#3CD85E", color: "#000000", fontSize: "80px", lineHeight: "97px", textAlign: "center", width: "628px" }}>Arena</div>
       <div style={{ padding: "26px", display: "flex", gap: "20px" }}>
         <div style={{ backgroundColor: "#EC6363", width: "409px", height: "364px", position: "relative" }}>
-          <div style={{ backgroundColor: "#D9D9D9", fontSize: "20px", color: "#000000", padding: "5px 18px", position: "absolute", top: "-5px", left: "10px" }}>Filter by: Alive</div>
+          <div style={{ backgroundColor: "#D9D9D9", fontSize: "20px", color: "#000000", padding: "5px 18px", position: "absolute", top: "-5px", left: "10px" }}>Filter by:&nbsp;
+            {turtleFilter == "alive" && <span style={{ cursor: "pointer" }} onClick={() => setTurtleFilter("dead")}>Alive</span>}
+            {turtleFilter == "dead" && <span style={{ cursor: "pointer" }} onClick={() => setTurtleFilter("alive")}>Dead</span>}
+          </div>
           {loading ? <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>Loading...</div> :
             <div style={{ display: "grid", gridTemplateColumns: "25% 25% 25% 25%", padding: "50px" }}>
-              {aliveTurtles.map(turtle => {
-                return (<div key={turtle.name} style={{ padding: "5px", cursor: "pointer" }} onClick={() => setSelectedTurtle(turtle)}>
-                  <img src={turtle.image} alt={`turtle`} />
+              {turtleFilter == "alive" && aliveTurtles.map(turtle => {
+                return (<div key={turtle.name} onClick={() => setSelectedTurtle(turtle)} className={[selectedTurtle?.name == turtle.name && "turtle_active", "player_image_wrapper"].join(" ")}>
+                  <img src={turtle.image} alt={`turtle`} className="player_image" />
+                </div>);
+              })}
+              {turtleFilter == "dead" && deadTurtles.map(turtle => {
+                return (<div key={turtle.name} onClick={() => setSelectedTurtle(turtle)} className={[selectedTurtle?.name == turtle.name && "turtle_active", "player_image_wrapper"].join(" ")}>
+                  <img src={turtle.image} alt={`turtle`} className="player_image" />
                 </div>);
               })}
             </div>}
         </div>
         <div style={{ backgroundColor: "#89AEF7", width: "409px", height: "364px", position: "relative" }}>
-          <div style={{ backgroundColor: "#D9D9D9", fontSize: "20px", color: "#000000", padding: "5px 18px", position: "absolute", top: "-5px", left: "10px" }}>Filter by: Alive</div>
+          <div style={{ backgroundColor: "#D9D9D9", fontSize: "20px", color: "#000000", padding: "5px 18px", position: "absolute", top: "-5px", left: "10px" }}>Filter by:&nbsp;
+            {rabbitFilter == "alive" && <span style={{ cursor: "pointer" }} onClick={() => setRabbitFilter("dead")}>Alive</span>}
+            {rabbitFilter == "dead" && <span style={{ cursor: "pointer" }} onClick={() => setRabbitFilter("alive")}>Dead</span>}
+          </div>
           {loading ? <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>Loading...</div> :
             <div style={{ display: "grid", gridTemplateColumns: "25% 25% 25% 25%", padding: "50px" }}>
-              {aliveRabbits.map(rabbit => {
-                return (<div key={rabbit.name} style={{ padding: "5px", cursor: "pointer" }} onClick={() => setSelectedRabbit(rabbit)}>
-                  <img src={rabbit.image} alt={`rabbit`} />
+              {rabbitFilter == "alive" && aliveRabbits.map(rabbit => {
+                return (<div key={rabbit.name} onClick={() => setSelectedRabbit(rabbit)} className={[selectedRabbit?.name == rabbit.name && "rabbit_active", "player_image_wrapper"].join(" ")}>
+                  <img src={rabbit.image} alt={`rabbit`} className="player_image" />
+                </div>);
+              })}
+              {rabbitFilter == "dead" && deadRabbits.map(rabbit => {
+                return (<div key={rabbit.name} onClick={() => setSelectedRabbit(rabbit)} className={[selectedRabbit?.name == rabbit.name && "rabbit_active", "player_image_wrapper"].join(" ")}>
+                  <img src={rabbit.image} alt={`rabbit`} className="player_image" />
                 </div>);
               })}
             </div>}
         </div>
       </div>
       <div style={{ background: `url(${BattleBackground})`, width: "877px", height: "524px", border: "10px solid #06EA61", position: "relative", display: "flex", flexDirection: "column", alignItems: "center" }}>
+        {isFighting && <img src={FireAnimation} alt="fire" style={{ position: "absolute", zIndex: 3 }} width="100%" height="100%" />}
         <div style={{ backgroundColor: "#A8BAAA", color: "#000", position: "absolute", padding: "6px 8px", top: "-9px" }}>
-          <span style={{ fontSize: "20px" }}>Attack</span>
-          <span style={{ fontSize: "20px", color: "#0047FC" }}>Rabbit</span>
-          <span style={{ fontSize: "20px" }}>Number</span>
-          <span style={{ fontSize: "20px", color: "#0047FC" }}>45</span>
-          <span style={{ fontSize: "20px" }}>With</span>
-          <span style={{ fontSize: "20px", color: "#FF1616" }}>Turtle</span>
-          <span style={{ fontSize: "20px" }}>Number</span>
-          <span style={{ fontSize: "20px", color: "#FF1616" }}>12</span>
+          {selectedRabbit && <>
+            <span style={{ fontSize: "20px" }}>Attack&nbsp;</span>
+            <span style={{ fontSize: "20px", color: "#0047FC" }}>Rabbit&nbsp;</span>
+            <span style={{ fontSize: "20px" }}>Number&nbsp;</span>
+            <span style={{ fontSize: "20px", color: "#0047FC" }}>{selectedRabbit?.name.slice(selectedRabbit?.name.indexOf("#") + 1)}</span>
+            <span style={{ fontSize: "20px" }}>&nbsp;With&nbsp;</span>
+          </>}
+          {selectedTurtle && <>
+            <span style={{ fontSize: "20px", color: "#FF1616" }}>Turtle&nbsp;</span>
+            <span style={{ fontSize: "20px" }}>Number&nbsp;</span>
+            <span style={{ fontSize: "20px", color: "#FF1616" }}>{selectedTurtle?.name.slice(selectedTurtle?.name.indexOf("#") + 1)}</span>
+          </>}
         </div>
         <div style={{ position: "absolute", top: "50%", transform: "translateY(-50%)", display: "flex", gap: "65px" }}>
           <div style={{ width: "333px", height: "333px", background: "linear-gradient(139deg, rgba(148,74,74,1) 0%, rgba(231,121,121,1) 35%, rgba(217,113,119,1) 55%, rgba(150,74,111,1) 100%)" }}>
@@ -95,7 +142,7 @@ function Arena({ info, mintInfo }) {
           </div>
         </div>
         <div style={{ position: "absolute", bottom: "-9px", padding: "20px 50px", backgroundColor: "#348546" }}>
-          <button style={{ padding: "5px 20px", fontSize: "20px", backgroundColor: "#D9D9D9", border: "none" }}>Fight!</button>
+          <button style={{ padding: "5px 20px", fontSize: "20px", backgroundColor: "#D9D9D9", border: "none", cursor: "pointer" }} disabled={!(selectedRabbit && selectedTurtle)} onClick={startFighting}>Fight!</button>
         </div>
       </div>
       <div style={{ position: "relative", backgroundColor: "#E5FDEA", width: "510px", height: "228px", marginTop: "50px", padding: "15px 40px", display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -117,9 +164,9 @@ function Arena({ info, mintInfo }) {
             <div style={{ height: "350px", overflow: "scroll", display: "flex", flexDirection: "column", gap: "10px" }}>
               {players.map((player, index) => {
                 return <div style={{ background: "linear-gradient(90deg, #02a2fc, #0255c8)", padding: "5px 20px", borderRadius: "10px", display: "flex", justifyContent: "center" }}>
-                  <span style={{fontSize: "16px"}}>{`${index + 1}.`}</span>
-                  <span style={{fontSize: "16px"}}>{player.name}</span>
-                  <span style={{fontSize: "16px", padding: "0 10px"}}>{player.kills}</span>
+                  <span style={{ fontSize: "16px" }}>{`${index + 1}.`}</span>
+                  <span style={{ fontSize: "16px" }}>{player.name}</span>
+                  <span style={{ fontSize: "16px", padding: "0 10px" }}>{player.kills}</span>
                 </div>
               })}
             </div>
