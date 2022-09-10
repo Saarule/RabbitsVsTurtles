c635e10c981df58f057574353602b4aac79c4ae7
Saarule <saaryazdi@gmail.com>
2022-09-11 01:06:53 +0300

    frontend updated

--- frontend/src/components/Minter.js
+++ frontend/src/components/Minter.js
@@ -722,11 +722,11 @@ function Minter() {
 
       <div style={{color:"#ffffff"}}>
       <div style={{ color:"#c2feff", height: 20, paddingLeft:40, paddingRight: window.innerWidth > 650 ? 0 : 40  ,paddingTop:50, paddingBottom:60, fontSize:40, letterSpacing:1.4, fontFamily:"slapstickShaded", textAlign:"center"}}> Duty is calling!</div>
-      <div style={{ height: 20, paddingTop:0, paddingBottom: window.innerWidth > 400 ? 25: 45, fontSize:20, letterSpacing:1.2, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}> Ready to take your part at the greatest war between the rabbits and the turtles?</div>
-      <div style={{ height: 20, paddingTop:0, paddingBottom:25, fontSize:20, letterSpacing:1.2, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}> Rabbits Vs. Turtles is 100% on-chain, dynamic, NFT Multiplayer game. </div>
+      <div style={{ height: 20, paddingTop:0, paddingBottom: window.innerWidth > 400 ? 35: 55, fontSize:20, letterSpacing:1.2, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}> Ready to take your part at the greatest war between the rabbits and the turtles?</div>
+      <div style={{ height: 20, paddingTop:0, paddingBottom:35, fontSize:20, letterSpacing:1.2, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}> Rabbits Vs. Turtles is 100% on-chain, dynamic, NFT Multiplayer game. </div>
       <div style={{ height: 20, paddingTop:0, paddingBottom:55, fontSize:20, letterSpacing:1.2, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}> The NFTs change dynamically as the game progress and each NFT mirrors the complete status of the player in the game!</div>
       {/* <div style={{ color:"#64eb50", height: 20, paddingTop:20, paddingBottom:45, fontSize:25, letterSpacing:1.1, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}>Connect your wallet to the Polygon network, mint your NFT, and join the game!</div> */}
-      <div style={{ background: "-webkit-linear-gradient(270deg, #0afafa, #0cebeb, #20e3b2, #29ffc6)",WebkitTextStroke:"0.001px #a6c9ff", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent", height: 20, paddingTop:40, paddingBottom:70, fontSize:26, letterSpacing:1.3, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}>Connect your wallet to the Polygon network, mint your NFT, and join the game!</div>
+      <div style={{ background: "-webkit-linear-gradient(270deg, #0afafa, #0cebeb, #20e3b2, #29ffc6)",WebkitTextStroke:"0.001px #a6c9ff", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent", height: 70, paddingTop:40, paddingBottom:70, fontSize:26, letterSpacing:1.3, lineHeight: 1.1, fontFamily:"slapstick", textAlign:"center"}}>Connect your wallet to the Polygon network, mint your NFT, and join the game!</div>
 
       </div>
       
