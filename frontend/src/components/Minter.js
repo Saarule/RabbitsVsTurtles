0c33b33b3d16a971ea646c9307bde3f84f8e9cf5
Saarule <saaryazdi@gmail.com>
2022-09-11 01:26:36 +0300

    frontend updated

--- frontend/src/components/Minter.js
+++ frontend/src/components/Minter.js
@@ -657,7 +657,7 @@ function Minter() {
       ) : (
     <div className="page" >
        {/* ************** Counters starts here! ************** */}
-      <div style={{ display: "grid", paddingTop:105, gridTemplateColumns: "repeat(2, 10fr)", gridGap: 90 ,paddingBottom: 40}}>
+      <div style={{ display: "grid", paddingTop:105, gridTemplateColumns: "repeat(2, 10fr)", gridGap: window.innerWidth > 400 ? 90 : 20 ,paddingBottom: 40}}>
       <div>
         <div>
           <div style={{color:"#ebc596", fontSize: window.innerWidth > 408 ? 20 : 13}}>
